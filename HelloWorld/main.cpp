484384537a5f70113719c983a9f32a12fb063f1a
GreenBean657 <greenbean657@proton.me>
2025-04-02 15:21:36 -0600

    Moved Structure

---
+++ HelloWorld/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    cout << "Git is now tracking this project!" << endl;
+
+    int age;
+    std::cout << "Enter Age: " << std::endl;
+    std::cin >> age;
+    std::cout << "Hello Person who is " << age << std::endl;
+    return age;
+}
