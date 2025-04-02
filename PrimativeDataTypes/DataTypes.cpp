820abf6d7c9e60da81ca1cf4e5fb251533e1f4b0
GreenBean657 <greenbean657@proton.me>
2025-04-02 15:56:09 -0600

    Added primative Data Types

---
+++ PrimativeDataTypes/DataTypes.cpp
@@ -0,0 +1,80 @@
+#include <chrono>
+#include <iostream>
+using namespace std;
+
+
+std::array<float, 3> calculateIncomes(const float wage, const float totalHours, const float TAX) {
+
+    std::array<float, 3> incomes{};
+
+    incomes[0] = totalHours * wage;
+    incomes[1] = incomes[0] * TAX;
+    incomes[2] = incomes[0] - incomes[1];
+
+    return incomes;
+}
+
+
+int main() {
+
+    string name;
+    char initial;
+    short age;
+    unsigned int zipcode;
+    float wage;
+    short daysWorked;
+    float hoursWorkedPerDay[7];
+    float totalHours = 0;
+
+    const float TAX = 0.1f;
+
+    std::cout << "Enter First Name: ";
+    std::cin >> name;
+
+    std::cout << "Enter Initial of last name: ";
+    std::cin >> initial;
+
+    std::cout << "Enter Age: ";
+    std::cin >> age;
+    bool isAdult = (age >= 18);
+
+    std::cout << "Enter Zipcode: ";
+    std::cin >> zipcode;
+
+    std::cout << "Enter hourly wage: ";
+    std::cin >> wage;
+
+    std::cout << "Enter number of days worked (max 7): ";
+    std::cin >> daysWorked;
+
+    for (int i = 0; i < daysWorked; i++) {
+        std::cout << "Enter hours worked on day " << (i + 1) << ": ";
+        std::cin >> hoursWorkedPerDay[i];
+        totalHours += hoursWorkedPerDay[i];
+    }
+
+    std::array<float, 3> incomes = calculateIncomes(wage, totalHours, TAX);
+
+    std::cout << std::format(R"(
+    Employee Payroll Summary
+    ------------------------
+    Name: {} {}.
+    Age: {} (Adult: {})
+    ZipCode: {}
+    Hourly Wage: ${.2f}
+    Total Hours Worked: {}
+    Gross Income: ${:.2f}
+    Tax Amount: ${.2f}
+    Net Income: ${.2f}
+
+    )",
+    name, initial,
+    age, (isAdult ? "Yes" : "No"),
+    zipcode,
+    wage,
+    totalHours,
+    incomes[0],
+    incomes[1],
+    incomes[2]
+    );
+}
